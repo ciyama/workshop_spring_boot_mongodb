ad02e3aa3a998d9990eeb1de5e566ce0fc5bf1d9
Celio Iyama <celio.iyama@gmail.com>
2021-07-30 10:15:53 -0300

    Obtendo um usuario por id

--- src/main/java/com/ciyama/wsmongo/services/UserService.java
+++ src/main/java/com/ciyama/wsmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ciyama.wsmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ciyama.wsmongo.domain.User;
 import com.ciyama.wsmongo.repository.UserRepository;
+import com.ciyama.wsmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
