b53562dbc8f27f03b93b42f3dd688a47c2f5d3c4
Celio Iyama <celio.iyama@gmail.com>
2021-07-31 11:01:44 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/ciyama/wsmongo/resources/UserResource.java
+++ src/main/java/com/ciyama/wsmongo/resources/UserResource.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ciyama.wsmongo.domain.Post;
 import com.ciyama.wsmongo.domain.User;
 import com.ciyama.wsmongo.dto.UserDTO;
 import com.ciyama.wsmongo.services.UserService;
@@ -58,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET) // ou @GetMapping
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
