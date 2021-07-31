44d8caf58bb90eebaf6453edd1d17501ead74851
Celio Iyama <celio.iyama@gmail.com>
2021-07-31 00:33:33 -0300

    Atualização de usuário com PUT

--- src/main/java/com/ciyama/wsmongo/resources/UserResource.java
+++ src/main/java/com/ciyama/wsmongo/resources/UserResource.java
@@ -38,8 +38,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
-	//@RequestMapping(method=RequestMethod.POST)
-	@PostMapping
+	@RequestMapping(method=RequestMethod.POST) //@PostMapping
  	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
@@ -50,6 +49,14 @@ public class UserResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE) 
 	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT) //@PutMapping
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/ciyama/wsmongo/services/UserService.java
+++ src/main/java/com/ciyama/wsmongo/services/UserService.java
@@ -31,9 +31,22 @@ public class UserService {
 	}
 	
 	public void delete(String id) {
+		findById(id);
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
