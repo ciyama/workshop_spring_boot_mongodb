3215abdb632ccb0b7b6000dafdaf89b04e5fcb93
Celio Iyama <celio.iyama@gmail.com>
2021-07-31 11:38:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/ciyama/wsmongo/domain/Post.java
+++ src/main/java/com/ciyama/wsmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.ciyama.wsmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ciyama.wsmongo.dto.AuthorDTO;
+import com.ciyama.wsmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,9 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	
 	public Post() {
 	}
@@ -72,6 +78,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,7 +103,6 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
 	
 
 }
